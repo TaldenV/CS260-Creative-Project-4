9f17d2b09da6ff71531031cfcba635b571de9c38
TJ VanderLouw <taldenv@byu.edu>
2021-12-04 14:28:26 -0700

    removed footer div

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -26,24 +26,22 @@
     <router-view />
 
     <!-- Footer -->
-    <div class="footer">
-      <b-navbar
-        class="fixed-bottom d-flex justify-content-between"
-        variant="light"
-        type="light"
-      >
-        <b-navbar-nav class="p-2">
-          <b-nav-item href="https://github.com/mg35/cp5.git" active
-            >Github</b-nav-item
-          >
-        </b-navbar-nav>
-        <b-navbar-nav class="p-2">
-          <b-nav-text>
-            Created by TJ VanderLouw, Milvia Goulding, and Melissa Morris
-          </b-nav-text>
-        </b-navbar-nav>
-      </b-navbar>
-    </div>
+    <b-navbar
+      class="fixed-bottom d-flex justify-content-between"
+      variant="light"
+      type="light"
+    >
+      <b-navbar-nav class="p-2">
+        <b-nav-item href="https://github.com/mg35/cp5.git" active
+          >Github</b-nav-item
+        >
+      </b-navbar-nav>
+      <b-navbar-nav class="p-2">
+        <b-nav-text>
+          Created by TJ VanderLouw, Milvia Goulding, and Melissa Morris
+        </b-nav-text>
+      </b-navbar-nav>
+    </b-navbar>
   </div>
 </template>
 
@@ -54,5 +52,52 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  box-sizing: border-box;
+  font: 14px 'Lucida Grande', Helvetica, Arial, sans-serif;
+  min-height: 100vh;
+  margin: 0;
+  position: relative;
+}
+
+img {
+  width: 100%;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+}
+
+.column > * {
+  margin-bottom: 20px;
+}
+
+a {
+  color: #00b7ff;
+}
+
+.addPage {
+  display: flex;
+  justify-content: center;
+}
+
+.add {
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  width: 66%;
+  justify-content: center;
+  align-content: center;
+}
+
+.notesAdded {
+  border-style: groove;
+}
+
+h2 {
+  font-size: 16px;
 }
 </style>
